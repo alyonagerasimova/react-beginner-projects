8f94e17f4f4949642a6ac63c3a4beb12388231ec
Alena Gerasimova <alena.gerasimova1999@gmail.com>
2022-09-10 13:35:34 +0400

    init modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+
+function Modal({ isOpen, setIsOpen }) {
+  return (
+    <div className={`overlay animated ${isOpen ? 'show' : ''}`}>
+      <div className="modal">
+        <svg
+          height="200"
+          viewBox="0 0 200 200"
+          width="200"
+          onClick={() => setIsOpen(false)}
+        >
+          <title />
+          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+        </svg>
+        <img
+          src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+          alt="gif"
+        />
+      </div>
+    </div>
+  );
+}
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="open-modal-btn" onClick={() => setIsOpen(true)}>
+        ✨ Открыть окно
+      </button>
+      {isOpen && <Modal isOpen={isOpen} setIsOpen={setIsOpen} />}
     </div>
   );
 }
